514deeca48547e575d3f529ed597e7a2bc08b97a
demo55oo <85605417+demo55oo@users.noreply.github.com>
2023-04-18 21:57:07 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,10 +144,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ('EMAIL_ID')
-EMAIL_HOST_PASSWORD = ('EMAIL_PW')
+EMAIL_HOST_USER = 'adhammansour886@gmail.com'
+EMAIL_HOST_PASSWORD = 'amkpxllkuqygpezy'
 
-DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
 
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
